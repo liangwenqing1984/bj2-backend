24553adedb3e49eba15cfc4c002ec452dd563c24
xiaoy597 <xiaoy597@qq.com>
2019-06-29 17:03:34 +0800

    calculate uuid for new tables.

---
+++ metadata-sync/gen-uuid-script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "use tmpdb;"
+echo "drop table if exists data_tbl_uuid;"
+echo "create table data_tbl_uuid(data_tblid int, data_tbl_uuid varchar(60), primary key(data_tblid));"
+echo -n "insert into data_tbl_uuid values "
+
+first_record=1
+while read tblid
+do
+	if [ $first_record -eq 1 ]; then
+		first_record=0
+	else
+		echo -n ","
+	fi
+	echo -n "($tblid, '"
+	echo -n `cat /proc/sys/kernel/random/uuid`
+	echo -n "')"
+done
+echo ";"
+
--- metadata-sync/metadata-sync.sh
+++ metadata-sync/metadata-sync.sh
@@ -1,37 +1,51 @@
 #!/bin/bash
 
-METASTORE_DB_HOST=10.1.1.175
-METASTORE_DB_USER=root
-METASTORE_DB_PASS=root
+METASTORE_DB_HOST=localhost
+METASTORE_DB_USER=cleanse_sys
+METASTORE_DB_PASS=cleanse_sys
 METASTORE_DB_NAME=metastore
 
 CLEANSE_DB_HOST=localhost
 CLEANSE_DB_USER=cleanse_sys
 CLEANSE_DB_PASS=cleanse_sys
-CLEANSE_METADB_NAME=metastore;
+CLEANSE_METADB_NAME=metastore_sync;
 CLEANSE_DB_NAME=cleanse_db;
 
 CUR_DATE=`date +%Y-%m-%d`
 
 echo "Begin processing ..."
 
+echo "Dumping metadata from metastore source ..."
 mysqldump -h $METASTORE_DB_HOST -u$METASTORE_DB_USER -p$METASTORE_DB_PASS \
 $METASTORE_DB_NAME columns_v2 tbls dbs partitions partition_params sds table_params > metastore.sql
 
-cat metastore-sync.sql|sed "s/\${METASTORE_DB}/$CLEANSE_METADB_NAME/g;s/\${CLEANSE_DB}/$CLEANSE_DB_NAME/g" > metastore-sync.sql.run
+cat metastore-sync-1.sql|sed "s/\${METASTORE_DB}/$CLEANSE_METADB_NAME/g;s/\${CLEANSE_DB}/$CLEANSE_DB_NAME/g" > metastore-sync-1.sql.run
+cat metastore-sync-2.sql|sed "s/\${METASTORE_DB}/$CLEANSE_METADB_NAME/g;s/\${CLEANSE_DB}/$CLEANSE_DB_NAME/g" > metastore-sync-2.sql.run
 
-echo "Loading metadata ..."
+echo "Loading metadata to cleanse database ..."
 date
 mysql -h $CLEANSE_DB_HOST -u$CLEANSE_DB_USER -p$CLEANSE_DB_PASS << END
 use $CLEANSE_METADB_NAME;
 source metastore.sql;
 END
 
-echo "Syncronizing metadata ..."
+echo "Syncronizing metadata phase 1 ..."
 date
 mysql -h $CLEANSE_DB_HOST -u$CLEANSE_DB_USER -p$CLEANSE_DB_PASS << END
-source metastore-sync.sql.run
+source metastore-sync-1.sql.run
+END
 
+echo "Calculating Table UUID ..."
+mysql -h $CLEANSE_DB_HOST -u$CLEANSE_DB_USER -p$CLEANSE_DB_PASS  -D tmpdb -N -e 'select data_tblid from data_tbl where data_tbl_uuid is null' | ./gen-uuid-script.sh > load-tbl-uuid.sql
+mysql -h $CLEANSE_DB_HOST -u$CLEANSE_DB_USER -p$CLEANSE_DB_PASS  < load-tbl-uuid.sql
+
+echo "Syncronizing metadata phase 2 ..."
+date
+mysql -h $CLEANSE_DB_HOST -u$CLEANSE_DB_USER -p$CLEANSE_DB_PASS << END
+source metastore-sync-2.sql.run
+END
+
+mysql -h $CLEANSE_DB_HOST -u$CLEANSE_DB_USER -p$CLEANSE_DB_PASS << END
 use $CLEANSE_DB_NAME;
 
 select count(*) as TABLE_ADDED from data_tbl where create_dt = "$CUR_DATE";
