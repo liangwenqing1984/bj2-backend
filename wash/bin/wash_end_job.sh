5a5b77203d1d169e50b0ac80fee3110e4f3eae36
liangwenqing1984 <liangwenqing1984@126.com>
2019-07-03 09:53:13 +0800

    change some error

--- wash/bin/wash_end_job.sh
+++ wash/bin/wash_end_job.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-CURR_TIME=$(date "+%Y-%m-%d %H:%M:%S"
-)echo $CURR_TIME "所有清洗作业完成！"
+
+CURR_TIME=$(date "+%Y-%m-%d %H:%M:%S")
+echo $CURR_TIME "所有清洗作业完成！"
--- wash/bin/wash_start_job.sh
+++ wash/bin/wash_start_job.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-CURR_TIME=$(date "+%Y-%m-%d %H:%M:%S"
-)echo $CURR_TIME "所有清洗作业触发作业完成！"
+
+CURR_TIME=$(date "+%Y-%m-%d %H:%M:%S")
+echo $CURR_TIME "所有清洗作业触发作业完成！"
